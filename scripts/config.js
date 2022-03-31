a06bb62179fa8048da7ce2f26216d50b4dcfea80
Adnan Hussain <adnanrajput42@gmail.com>
2022-04-01 01:20:40 +0500

    chore: setup config and api

---
+++ scripts/config.js
@@ -0,0 +1,5 @@
+var config = {
+  api_key: "a57da5496baba12441847604c437d99e",
+  api_base_url: "https://api.themoviedb.org/3/",
+  image_base_url: "https://image.tmdb.org/t/p/w1280",
+};
---
+++ scripts/imageUtil.js
@@ -0,0 +1,12 @@
+const getImageUrl = (basePath) => {
+  return `${config.image_base_url}${basePath}`;
+};
+
+// The idea to use it?
+// let say movieObject is a movie data which has key poster_path which is a relative path of image
+// but we need the whole path of the image, we can get that by appending image_base_url given by api
+
+// Now lets assume poster_path is "/w1280/qsdjk9oAKSQMWs0Vt5Pyfh6O4GZ.jpg"
+// After using below function
+// getImageUrl(movieObject.poster_path)
+// Will return something like https://image.tmdb.org/t/p/w1280/qsdjk9oAKSQMWs0Vt5Pyfh6O4GZ.jpg
