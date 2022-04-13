1d492cc415bfadce3e82714f500fc090beacba36
Tooba Aftab <tooba.aftab845@gmail.com>
2022-04-14 01:01:24 +0500

    update display of movies

--- scripts/app.js
+++ scripts/app.js
@@ -1,39 +1,33 @@
 const movies = document.getElementById("movies");
 const singlemovie = document.getElementById("movie");
 
-function getImagesList(element){
+displayMoviesList();
+//getMovies();
+
+function getMoviesList(element){
     let displayImages = '';
     for (let index = 0; index < element.movies.length; index++) {
         displayImages = displayImages + `<div class="card-img col-md-2 mb-3 bg-dark">
         <img src="${element.movies[index].image}"/>
         </div>`
-        // console.log(displayImages);
     }
     return displayImages;
 }
 
-function displayList() {
-    // console.log(data[1].movies);
-    // movies.innerHTML = data?.map((movieData,index) => getSingleMovie(movieData,index)).join("")
-
+async function displayMoviesList() {
     let displayCategory = '';
-    
-    data.forEach(element => {
-        displayCategory = displayCategory + `<h2>${element.category}</h2><div class="d-flex flex-row flex-wrap justify-content-between">`
-        displayCategory = displayCategory + getImagesList(element);
-        displayCategory = displayCategory + `</div>`;
-        console.log(displayCategory)
-        // console.log(getImagesList(element));
-        
+    const moviesList = await getMovies();
+    const displayMovies = moviesList.map((currentElement) => {
+        displayCategory = displayCategory + `<h2>${currentElement.category}</h2>
+    <div class="d-flex flex-row flex-wrap">`;
+    currentElement.movies.forEach(element => {
+        const {id,image,title} = element;
+        displayCategory = displayCategory + `<div class="card-img col-md-2 mb-2 bg-dark">
+        <img src="${image}"/>
+        </div>`;
     });
-
+    displayCategory = displayCategory + `</div>`;
+    })
+    
     movies.innerHTML = displayCategory;
-}
-displayList()
-
-function getSingleMovie(movieData,index){
-    // console.log(index);
-//     const movieDiv = `<div class="card-img col-md-2 mb-3 bg-dark">
-//     <img src="${movieData[index]['movies']}">
-//   </div>`
 }
