e7fe9fb5245b5de84ad787b5f0649fdcd5e46a68
Tooba Aftab <tooba.aftab845@gmail.com>
2022-04-14 02:34:32 +0500

    refactor loop to map

--- scripts/app.js
+++ scripts/app.js
@@ -2,32 +2,21 @@ const movies = document.getElementById("movies");
 const singlemovie = document.getElementById("movie");
 
 displayMoviesList();
-//getMovies();
 
 function getMoviesList(element){
-    let displayImages = '';
-    for (let index = 0; index < element.movies.length; index++) {
-        displayImages = displayImages + `<div class="card-img col-md-2 mb-3 bg-dark">
-        <img src="${element.movies[index].image}"/>
-        </div>`
-    }
-    return displayImages;
+    return element.movies?.map(currentElement => {
+        return `<div class="card-img col-md-2 mb-2 bg-dark">
+        <img src="${currentElement.image}"/>
+        </div>`;
+    }).join("");
 }
 
 async function displayMoviesList() {
-    let displayCategory = '';
     const moviesList = await getMovies();
-    const displayMovies = moviesList.map((currentElement) => {
-        displayCategory = displayCategory + `<h2>${currentElement.category}</h2>
-    <div class="d-flex flex-row flex-wrap">`;
-    currentElement.movies.forEach(element => {
-        const {id,image,title} = element;
-        displayCategory = displayCategory + `<div class="card-img col-md-2 mb-2 bg-dark">
-        <img src="${image}"/>
-        </div>`;
-    });
-    displayCategory = displayCategory + `</div>`;
-    })
-    
-    movies.innerHTML = displayCategory;
+    const displayMovies = moviesList?.map((currentElement) => {
+        return `<h2>${currentElement.category}</h2>
+    <div class="d-flex flex-row flex-wrap"> ${getMoviesList(currentElement)} </div>`;
+    }).join("");
+
+    movies.innerHTML = displayMovies;
 }
